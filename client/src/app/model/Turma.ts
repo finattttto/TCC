48519accd4ab5ff3a5f63cb6e46f4f8c72508346
Luan Finatto <luanfinatto6@gmail.com>
2025-01-30 18:54:18 -0300

    Atualizando nome da aplicação

--- client/src/app/model/Turma.ts
+++ client/src/app/model/Turma.ts
@@ -2,8 +2,8 @@ import { AbstractModel } from "./AbstractModel";
 import { Usuario } from "./Usuario";
 
 export class Turma extends AbstractModel {
-    nome: string;
-    codigo: string;
+    nome: string = "";
+    codigo: string = "";
     dataGeracaoCodigo: Date;
     palavras: number[];
     usuario: Usuario;
